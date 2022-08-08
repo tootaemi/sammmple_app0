b567b40738790778ef72c08d995475191e1f550d
tootaemi <108526267+tootaemi@users.noreply.github.com>
2022-08-08 13:17:33 +0000

    Initial Commit1

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -14,7 +14,7 @@ class ListsController < ApplicationController
   end
 
 
-  # 以下を追加
+ # 以下を追加
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
     list = List.new(list_params)
@@ -23,13 +23,28 @@ class ListsController < ApplicationController
     # 4. トップ画面へリダイレクト
      # 詳細画面へリダイレクト
     redirect_to list_path(list.id)  
-  
   end
-  
-  
+
+
+
+ def edit
+    @list = List.find(params[:id])
+  end
+
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)  
+  end
+
+
   private
-  # ストロングパラメータ
+  
   def list_params
     params.require(:list).permit(:title, :body)
   end
+  
 end
+
+
